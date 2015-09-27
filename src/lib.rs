a461ad1d56b275323018336deb3d6392db9075e1
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-09-27 14:17:34 -0700

    add c-string variants and test for escape sequences (that fails)

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,15 @@ use encoding::types::{EncoderTrap,Encoding};
 
 use rustc::plugin;
 
-fn expand<T>(encoding: &T, ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::TokenTree]) -> Box<MacResult> where T: Encoding {
+fn expand<T>(encoding: &T, ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::TokenTree], c_str: bool, size_in_bytes: isize) -> Box<MacResult> where T: Encoding {
     let text = match args {
-        [ast::TtToken(_, token::Literal(token::Lit::Str_(s), _))] => s.to_string(),
+        [ast::TtToken(_, token::Literal(token::Lit::Str_(s), _))] => s.as_str().to_string(),
         _ => {
             ct.span_err(sp, "argument should be a single string literal");
             return DummyResult::any(sp);
         }
     };
+    println!("ENCODING: text={:?}", text);
 
     // Encode the string
     let encoded = match encoding.encode(&text, EncoderTrap::Strict) {
@@ -34,19 +35,38 @@ fn expand<T>(encoding: &T, ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::Tok
     };
 
     // Generate the expression
-    let expr = ct.expr(sp, ast::Expr_::ExprVec(encoded
+    let mut bytes : Vec<P<ast::Expr>> = encoded
         .iter()
         .map(|v: &u8| -> P<ast::Expr> { ct.expr_u8(sp, *v) })
-        .collect()));
+        .collect();
+    if c_str {
+        for _ in 0..size_in_bytes {
+            bytes.push(ct.expr_u8(sp, 0));
+        }
+    }
+
+    let expr = ct.expr(sp, ast::Expr_::ExprVec(bytes));
     MacEager::expr(expr)
 }
 
+fn expand_c_utf8(ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::TokenTree]) -> Box<MacResult> {
+    expand(encoding::all::UTF_8, ct, sp, args, true, 1)
+}
+
+fn expand_c_utf16(ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::TokenTree]) -> Box<MacResult> {
+    expand(encoding::all::UTF_16LE, ct, sp, args, true, 2)
+}
+
+fn expand_c_utf16be(ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::TokenTree]) -> Box<MacResult> {
+    expand(encoding::all::UTF_16BE, ct, sp, args, true, 2)
+}
+
 fn expand_utf16(ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::TokenTree]) -> Box<MacResult> {
-    expand(encoding::all::UTF_16LE, ct, sp, args)
+    expand(encoding::all::UTF_16LE, ct, sp, args, false, 2)
 }
 
 fn expand_utf16be(ct: &mut ExtCtxt, sp: codemap::Span, args: &[ast::TokenTree]) -> Box<MacResult> {
-    expand(encoding::all::UTF_16BE, ct, sp, args)
+    expand(encoding::all::UTF_16BE, ct, sp, args, false, 2)
 }
 
 #[plugin_registrar]
@@ -54,4 +74,9 @@ pub fn plugin_registrar(reg: &mut plugin::Registry) {
     reg.register_macro("utf16", expand_utf16);
     reg.register_macro("utf16le", expand_utf16);
     reg.register_macro("utf16be", expand_utf16be);
+
+    reg.register_macro("c_utf8", expand_c_utf8);
+    reg.register_macro("c_utf16", expand_c_utf16);
+    reg.register_macro("c_utf16le", expand_c_utf16);
+    reg.register_macro("c_utf16be", expand_c_utf16be);
 }
--- tests/literals.rs
+++ tests/literals.rs
@@ -7,6 +7,14 @@ use encoding::all;
 use encoding::types::{Encoding,EncoderTrap,DecoderTrap};
 
 #[test]
+pub fn escape_sequences_are_properly_encoded() {
+    let runtime_encoded = all::UTF_16LE.encode("Te\r\nst", EncoderTrap::Strict).ok().unwrap();
+    let literal = utf16!("Te\r\nst");
+    assert_eq!("Te\r\nst", all::UTF_16LE.decode(&literal, DecoderTrap::Strict).ok().unwrap());
+    assert_eq!(runtime_encoded, literal);
+}
+
+#[test]
 pub fn utf16_encodes_as_expected() {
     let runtime_encoded = all::UTF_16LE.encode("Test", EncoderTrap::Strict).ok().unwrap();
     let literal = utf16!("Test");
@@ -31,4 +39,28 @@ pub fn utf16be_encodes_as_expected() {
     assert_eq!(runtime_encoded, literal);
     assert_eq!([0, 'T' as u8, 0, 'e' as u8, 0, 's' as u8, 0, 't' as u8], literal);
     assert_eq!("Test", all::UTF_16BE.decode(&literal, DecoderTrap::Strict).ok().unwrap());
+}
+
+#[test]
+pub fn c_utf16_encodes_as_expected() {
+    let literal = c_utf16!("Test");
+    assert_eq!(['T' as u8, 0, 'e' as u8, 0, 's' as u8, 0, 't' as u8, 0, 0, 0], literal);
+}
+
+#[test]
+pub fn c_utf16le_encodes_as_expected() {
+    let literal = c_utf16le!("Test");
+    assert_eq!(['T' as u8, 0, 'e' as u8, 0, 's' as u8, 0, 't' as u8, 0, 0, 0], literal);
+}
+
+#[test]
+pub fn c_utf16be_encodes_as_expected() {
+    let literal = c_utf16be!("Test");
+    assert_eq!([0, 'T' as u8, 0, 'e' as u8, 0, 's' as u8, 0, 't' as u8, 0, 0], literal);
+}
+
+#[test]
+pub fn c_utf8_encodes_as_expected() {
+    let literal = c_utf8!("Test");
+    assert_eq!(['T' as u8, 'e' as u8, 's' as u8, 't' as u8, 0], literal);
 }
